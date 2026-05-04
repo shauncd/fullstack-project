50acc32e6e6eb068eca34db2d9914078f469e69b
shauncd <shaunderstine@gmail.com>
2026-05-04 18:40:05 -0400

    add CMD in Dockerfile to launch server.js by default

--- server/start-node-server.sh
+++ server/start-node-server.sh
@@ -1,2 +1,2 @@
 docker build -t node-server . && \
-docker run -d --rm --name node-server --network my-network -p 127.0.0.1:3000:3000 node-server server.js
+docker run -d --rm --name node-server --network my-network -p 127.0.0.1:3000:3000 node-server
